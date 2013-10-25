b561d28bd5f8f3281a11c240823458bac6863867
dinidu sandaruwan <iamdinidu@gmail.com>
2013-10-25 23:28:16 +0530

    footer modified

--- application/views/footer.php
+++ application/views/footer.php
@@ -63,3 +63,9 @@
     </div>
   </div>
 </div>
+<div class="wrapper row4">
+  <div id="copyright" class="clear">
+    <p class="fl_left">Copyright &copy; 2013 - All Rights Reserved - <a href="#">Domain Name</a></p>
+    <p class="fl_right">Template by <a href="http://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
+  </div>
+</div>
--- application/views/index.php
+++ application/views/index.php
@@ -113,12 +113,7 @@
 </div>
 <!-- Footer -->
 <?php $this->load->view('footer'); ?>
-<div class="wrapper row4">
-  <div id="copyright" class="clear">
-    <p class="fl_left">Copyright &copy; 2013 - All Rights Reserved - <a href="#">Domain Name</a></p>
-    <p class="fl_right">Template by <a href="http://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
-  </div>
-</div>
+
 <!-- Scripts -->
 <script src="http://code.jquery.com/jquery-latest.min.js"></script>
 <script src="http://code.jquery.com/ui/1.10.1/jquery-ui.min.js"></script>

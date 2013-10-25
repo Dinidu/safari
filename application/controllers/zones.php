78edb8a01691459f1036fe76b52549c12c58d798
dinidu sandaruwan <iamdinidu@gmail.com>
2013-10-25 23:15:57 +0530

    files added

---
+++ application/controllers/zones.php
@@ -0,0 +1,22 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Zone extends CI_Controller {
+
+	// This will load the admin login page
+	public function residental()
+	{
+		$this->load->view('pages/full-width-content');
+	}
+	public function index()
+	{
+		
+	}
+	public function index()
+	{
+		
+	}
+	public function index()
+	{
+		
+	}
+}	
--- application/views/index.php
+++ application/views/index.php
@@ -18,8 +18,8 @@
 <div class="wrapper row1">
   <header id="header" class="full_width clear">
     <hgroup>
-      <h1><a href="index.html">RS-1200 Prototype 51</a></h1>
-      <h2>Free Responsive Template</h2>
+      <h1><a href="index.html">National Safari Park Srilanka</a></h1>
+      <h2>Ridiyagama, Hambanthota</h2>
     </hgroup>
     <div id="header-contact">
       <ul class="list none">
@@ -33,10 +33,10 @@
 <div class="wrapper row2">
   <nav id="topnav">
     <ul class="clear">
-      <li class="active"><a href="index.html" title="Homepage">Homepage</a></li>
-      <li><a class="drop" href="#" title="Pages">Pages</a>
+      <li class="active"><a href="index.html" title="Homepage">Home</a></li>
+      <li><a class="drop" href="#" title="Pages">Zones</a>
         <ul>
-          <li><a href="pages/full-width-content.html" title="Full Width Content">Full Width Content</a></li>
+          <!-- <li><a href="pages/full-width-content.html" title="Full Width Content">Full Width Content</a></li>
           <li><a href="pages/content-leftsidebar.html" title="Content + Left Sidebar">Content + Left Sidebar</a></li>
           <li><a href="pages/content-rightsidebar.html" title="Content + Right Sidebar">Content + Right Sidebar</a></li>
           <li><a href="pages/content-bothsidebars.html" title="Content + Both Sidebars">Content + Both Sidebars</a></li>
@@ -45,12 +45,18 @@
           <li><a href="pages/testimonials.html" title="Testimonials">Testimonials</a></li>
           <li><a href="pages/about-us.html" title="About Us">About Us</a></li>
           <li><a href="pages/team-member.html" title="Team Member">Team Member</a></li>
-          <li class="last-child"><a href="pages/faq.html" title="FAQ">FAQ</a></li>
+          <li class="last-child"><a href="pages/faq.html" title="FAQ">FAQ</a></li> -->
+          <li><a href="pages/team-member.html" title="Residental">Residental</a></li>
+          <li><a href="pages/team-member.html" title="Lion Zone">Lion Zone</a></li>
+          <li><a href="pages/team-member.html" title="African Zone">African Zone</a></li>
+          <li><a href="pages/team-member.html" title="Bengal Zone">Bengal Zone</a></li>
+          <li><a href="pages/team-member.html" title="Austrailian Zone">Austrailian Zone</a></li>
+          <li><a href="pages/team-member.html" title="Lepord + Srilankan Zone">Lepord + Srilankan Zone</a></li>
         </ul>
       </li>
-      <li><a class="drop" href="#" title="Elements">Elements</a>
+      <li><a class="drop" href="#" title="Drive Through">Drive Through</a>
         <ul>
-          <li><a href="elements/buttons.html" title="Buttons">Buttons</a></li>
+          <!-- <li><a href="elements/buttons.html" title="Buttons">Buttons</a></li>
           <li><a href="elements/alert-messages.html" title="Alert Messages">Alert Messages</a></li>
           <li><a href="elements/font-icons.html" title="Font Icons">Font Icons</a></li>
           <li><a href="elements/call-to-action.html" title="Call To Action">Call To Action</a></li>
@@ -60,12 +66,17 @@
           <li><a href="elements/accordions.html" title="Accordions">Accordions</a></li>
           <li><a href="elements/tabs.html" title="Tabs">Tabs</a></li>
           <li><a href="elements/toggles.html" title="Toggles">Toggles</a></li>
-          <li class="last-child"><a href="elements/pricing-tables.html" title="Pricing Tables">Pricing Tables</a></li>
+          <li class="last-child"><a href="elements/pricing-tables.html" title="Pricing Tables">Pricing Tables</a></li> -->
+          <li><a href="elements/toggles.html" title="Safari Map">Safari Map</a></li>
+          <li><a href="elements/toggles.html" title="Trip Schedule">Trip Shedules</a></li>
+          <li><a href="elements/toggles.html" title="Vehicles">Vehicles</a></li>
         </ul>
       </li>
-      <li><a class="drop" href="#" title="Portfolio Layouts">Portfolio Layouts</a>
+      <li><a class="drop" href="#" title="Visiter Facilities">Visiter Facilities</a>
         <ul>
-          <li><a href="portfolio-layouts/portfolio-overview-full-width-content.html" title="Full Width Portfolio">Full Width Portfolio</a></li>
+          <li><a href="elements/toggles.html" title="Car Park">Car Park</a></li>
+          <li><a href="elements/toggles.html" title="Ticket Counter">Ticket Counter</a></li>	
+          <!-- <li><a href="portfolio-layouts/portfolio-overview-full-width-content.html" title="Full Width Portfolio">Full Width Portfolio</a></li>
           <li><a href="portfolio-layouts/portfolio-overview-2columns.html" title="2 Column Grid">2 Column Grid</a></li>
           <li><a href="portfolio-layouts/portfolio-overview-2columns-leftsidebar.html" title="2 Column Grid + Left Sidebar">2 Col. Grid + Left Sidebar</a></li>
           <li><a href="portfolio-layouts/portfolio-overview-2columns-rightsidebar.html" title="2 Column Grid + Right Sidebar">2 Col. Grid + Right Sidebar</a></li>
@@ -74,11 +85,11 @@
           <li><a href="portfolio-layouts/portfolio-overview-3columns-leftsidebar.html" title="3 Column Grid + Left Sidebar">3 Col. Grid + Left Sidebar</a></li>
           <li><a href="portfolio-layouts/portfolio-overview-3columns-rightsidebar.html" title="3 Column Grid + Right Sidebar">3 Col. Grid + Right Sidebar</a></li>
           <li><a href="portfolio-layouts/portfolio-overview-3columns-bothsidebars.html" title="3 Column Grid + Both Sidebars">3 Col. Grid + Both Sidebars</a></li>
-          <li class="last-child"><a href="portfolio-layouts/portfolio-overview-4columns.html" title="4 Column Grid">4 Column Grid</a></li>
+          <li class="last-child"><a href="portfolio-layouts/portfolio-overview-4columns.html" title="4 Column Grid">4 Column Grid</a></li> -->
         </ul>
       </li>
-      <li><a class="drop" href="#" title="Gallery Layouts">Gallery Layouts</a>
-        <ul>
+      <li><a href="#" title="Gallery">Gallery</a>
+        <!-- <ul>
           <li><a href="gallery-layouts/gallery-full-width-content.html" title="Full Width Gallery">Full Width Gallery</a></li>
           <li><a href="gallery-layouts/gallery-2columns.html" title="2 Column Grid">2 Column Grid</a></li>
           <li><a href="gallery-layouts/gallery-2columns-leftsidebar.html" title="2 Column Grid + Left Sidebar">2 Col. Grid + Left Sidebar</a></li>
@@ -90,10 +101,10 @@
           <li><a href="gallery-layouts/gallery-3columns-bothsidebars.html" title="3 Column Grid + Both Sidebars">3 Col. Grid + Both Sidebars</a></li>
           <li><a href="gallery-layouts/gallery-4columns.html" title="4 Column Grid">4 Column Grid</a></li>
           <li class="last-child"><a href="gallery-layouts/gallery-5columns.html" title="5 Column Grid">5 Column Grid</a></li>
-        </ul>
+        </ul> -->
       </li>
-      <li><a href="#" title="Link Text">Link Text</a></li>
-      <li class="last-child"><a href="#" title="A Very Long Link Text">A Very Long Link Text</a></li>
+      <li><a href="#" title="Safari Blog">Safari Blog</a></li>
+      <li class="last-child"><a href="#" title="Contact Us">Contact Us</a></li>
     </ul>
   </nav>
 </div>
